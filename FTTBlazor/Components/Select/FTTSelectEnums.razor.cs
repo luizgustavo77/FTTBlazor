c091f0d7e140ef22dec0ce05849fec5f7ba138bb
Luiz Gustavo de Freitas <38074380+luizgustavo77@users.noreply.github.com>
2021-09-28 09:20:56 -0300

    Primeiro funcional selectenums

--- FTTBlazor/Components/Select/FTTSelectEnums.razor.cs
+++ FTTBlazor/Components/Select/FTTSelectEnums.razor.cs
@@ -2,26 +2,26 @@ using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FTTBlazor.Components.Select
 {
-
-
     public partial class FTTSelectEnums<T> : ComponentBase where T : struct, IConvertible
     {
-        public bool Show { get; set; }
+        [Parameter]
+        public string Title { get; set; }
 
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-        }
+        [Parameter]
+        public bool Disabled { get; set; } = false;
 
+        [Parameter]
+        public EventCallback OnItemSelected { get; set; }
 
-        private string strEnum;
+        [Parameter]
+        public string Size { get; set; } = "4";
 
-        private bool _IsReadOnly;
+        [Parameter]
+        public bool BorderRight { get; set; }
 
         [Parameter]
         public bool IsReadOnly
@@ -30,12 +30,14 @@ namespace FTTBlazor.Components.Select
             set { _IsReadOnly = value; }
         }
 
+        private bool _IsReadOnly;
 
         [Parameter]
         public T Value
         {
             get { return Enum.Parse<T>(strEnum); }
-            set {  
+            set
+            {
 
                 if (strEnum == value.ToString()) return;
 
@@ -58,26 +60,23 @@ namespace FTTBlazor.Components.Select
             }
         }
 
-        public string data_id { get; set; } = "00000000-0000-0000-0000-000000000000";
-
-
         [Parameter]
         public EventCallback<T> ValueChanged { get; set; }
 
+        [Parameter]
+        public bool Required { get; set; } = false;
 
+        public bool Show { get; set; }
+
+        private string strEnum;
+
+        public string data_id { get; set; } = "00000000-0000-0000-0000-000000000000";
 
         protected override async Task OnInitializedAsync()
         {
-
-
-
-
             await base.OnInitializedAsync();
-            
 
             DataSource = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
-
-          
 
             _id = Guid.NewGuid().ToString();
 
@@ -87,48 +86,27 @@ namespace FTTBlazor.Components.Select
             {
                 attributes.Add(new KeyValuePair<string, object>("required", "required"));
             }
-
-            // attributes.Add(new KeyValuePair<string, object>("data-desc", DataDesc));
-
         }
 
         private string _id { get; set; }
 
-        [Parameter]
-        public string Title { get; set; }
-
-        [Parameter]
-        public bool Disabled { get; set; } = false;
-
-
         public IEnumerable<T> DataSource { get; set; }
 
-        [Parameter]
-        public bool Required { get; set; } = false;
+        private Guid _dataid;
+
+        private string BorderRightCssClass { get { return BorderRight ? "nexto-border-right" : ""; } }
 
         private List<KeyValuePair<string, object>> attributes;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
         protected virtual bool ValidateCurrentValue(T value)
         {
             return true;
         }
 
-        private Guid _dataid;
-
-
-        [Parameter] public EventCallback OnItemSelected { get; set; }
-
-
-        [Parameter]
-        public string Size { get; set; } = "4";
-
-        [Parameter]
-        public bool BorderRight { get; set; }
-
-        private string BorderRightCssClass { get { return BorderRight ? "nexto-border-right" : ""; } }
-
-
-
-
     }
-
 }
