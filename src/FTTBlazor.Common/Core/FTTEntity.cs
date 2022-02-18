ebe1d81baaeb48a37e298f0aaacde602b3be4091
Luiz Gustavo de Freitas <38074380+luizgustavo77@users.noreply.github.com>
2022-02-18 13:20:03 -0300

    Melhorando o CRUD para ter mais informações de como aplicar

--- src/FTTBlazor.Common/Core/FTTEntity.cs
+++ src/FTTBlazor.Common/Core/FTTEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace FTTBlazor.Common.Core
 {
-    public class FTTEntity
+    public abstract class FTTEntity
     {
         public string Id { get; set; }
         public string Name { get; set; }
